f25832d71e914abc59357b771a2ca1f962fff56a
Mahmoudmkr <mahmoudmah23@gmail.com>
2023-03-29 04:42:32 +0300

    Initial commit

---
+++ myapp/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.conf.global_settings import AUTH_USER_MODEL
+
+# Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Status(models.IntegerChoices):
+    PENDING=1, 'pending'
+    COMPLETED=2, 'completed'
+    POSTPONED=3, 'postponed'
+    CANCELED=4, 'canceled'
+
+
+
+class Project(models.Model):
+    title=models.CharField(max_length=255)
+    description=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    status=models.IntegerField(
+        choices=Status.choices,
+        default=Status.PENDING
+    )
+    user=models.ForeignKey(
+        AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True
+    )
+
+    def __str__(self):
+        return self.title
+
+class Task(models.Model):
+    description=models.TextField()
+    is_completed=models.BooleanField(default=False)
+    project=models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
