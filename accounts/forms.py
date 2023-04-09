c759acf8fb0c6cc58c3b51a808f69bfc391ea9a1
Mahmoudmkr <mahmoudmah23@gmail.com>
2023-04-09 03:22:12 +0300

    registration & profile update

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import AuthenticationForm,UserCreationForm,UserChangeForm
 from django import forms
+from django.contrib.auth.models import User
 attrs={'class':'form-control'}
 
 
@@ -10,3 +11,27 @@ class UserLoginForm(AuthenticationForm):
 
     username=forms.CharField(label='username',widget=forms.TextInput(attrs=attrs))
     password=forms.PasswordInput(attrs=attrs)
+
+
+class RegisterCreateForm(UserCreationForm):
+
+    first_name=forms.CharField(label='first name',widget=forms.TextInput(attrs=attrs))
+    last_name=forms.CharField(label='last name',widget=forms.TextInput(attrs=attrs))
+    username=forms.CharField(label='username',widget=forms.TextInput(attrs=attrs))
+    email=forms.CharField(label='email',widget=forms.TextInput(attrs=attrs))
+    password1=forms.CharField(label='password',strip=False,widget=forms.PasswordInput(attrs=attrs))
+    password2=forms.CharField(label='password confirmation',strip=False,widget=forms.PasswordInput(attrs=attrs))
+
+    class Meta(UserCreationForm.Meta):
+        fields=['first_name','last_name' ,'username','email']
+
+class ProfileForm(UserChangeForm):
+    password=None
+    class Meta:
+        model=User
+        fields=['first_name','last_name','email']
+        widgets={
+            'first_name':forms.TextInput(attrs=attrs),
+            'last_name':forms.TextInput(attrs=attrs),
+            'email':forms.EmailInput(attrs=attrs)
+        }
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.views.generic import CreateView
+from .forms import RegisterCreateForm,ProfileForm
+from django.urls import reverse_lazy
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+class RegisterView(CreateView):
+    form_class =RegisterCreateForm
+    template_name = 'registration/register.html'
+
+    def get_success_url(self):
+        login(self.request,self.object)
+        return reverse_lazy('list')
+@login_required
+def edit_profile(request):
+    if request.method== 'POST':
+        form=ProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            redirect('profile')
+    form = ProfileForm(instance=request.user)
+    return render(request,'profile.html',{
+        'form':form
+    })
+
+
